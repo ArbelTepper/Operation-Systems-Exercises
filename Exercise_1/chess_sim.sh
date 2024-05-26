6ae005e8838900dc251d59999bbab0d01487d8cc
Arbel <arbel90@gmail.com>
2024-05-26 16:28:29 +0300

    Added a build_board and a print_board funtions.

--- Exercise_1/chess_sim.sh
+++ Exercise_1/chess_sim.sh
@@ -2,11 +2,53 @@
 
 # This script is used to run the chess simulation program
 
+declare -A board
+
+function create_board {
+  
+  #Initialize the board with dots
+  for ((i=1; i<=8; i++)); do
+    for ((j=0; j<=8; j++)); do
+      board[$i,$j]="."
+    done
+  done
+
+  # Set the row numbers
+  for ((i=1; i<=8; i++)); do
+    board[$i,0]=$[9-i]
+    board[$i,9]=$[9-i]
+  done
+
+  # Set the soldiers
+  for ((i=1; i<=8; i++)); do
+    board[2,$i]="p"
+    board[7,$i]="P"
+  done
+
+  board[1,1]="r"; board[1,2]="n"; board[1,3]="b"; board[1,4]="q"; board[1,5]="k"
+  board[1,6]="b"; board[1,7]="n"; board[1,8]="r"
+
+  board[8,1]="R"; board[8,2]="N"; board[8,3]="B"; board[8,4]="Q"; board[8,5]="K"
+  board[8,6]="B"; board[8,7]="N"; board[8,8]="R"
+}
+
+function print_board {
+  echo -n "  a b c d e f g h"
+  for ((i=0; i<=8; i++)); do
+    for ((j=0; j<=10; j++)); do
+      echo -n "${board[$i,$j]} " # Print the value of the board at position i,j
+    done
+    echo # Print a new line
+  done
+  echo "  a b c d e f g h"
+}
+
+# function update_board {}
+
 metadata=""
 pgn_moves=""
 empty_line_reached=false
 current_move=0
-
 
 # Read the file line by line and separate the metadata and the moves when the first empty line is found.
 while IFS= read -r line || [[ -n $line ]]
@@ -22,7 +64,6 @@ do
     metadata+="$line"$'\n'
   fi
 done < "$1"
-echo "$pgn_moves" > "pgn_moves.txt"
 
 # Parse the moves from the PGN file to UCI format
 uci_moves=$(python3 "parse_moves.py" "$pgn_moves")
